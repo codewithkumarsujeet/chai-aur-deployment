c0eb07e13561e5f29a017be17feee307a5f9d233
codewithkumarsujeet <104923531+codewithkumarsujeet@users.noreply.github.com>
2026-06-17 00:27:09 +0530

    version change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8000;
 
 app.get("/", (req, res) => {
-  return res.json({ message: "Server is healthy" });
+  return res.json({ message: "Server is healthy this is v 3.1" });
 });
 
 app.listen(PORT, (req, res) => {
