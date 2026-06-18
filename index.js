c253a35eb714733c0dffd7835827754d710d8235
codewithkumarsujeet <104923531+codewithkumarsujeet@users.noreply.github.com>
2026-06-19 01:15:04 +0530

    modified message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8000;
 
 app.get("/", (req, res) => {
-  return res.json({ message: "Server is healthy this is v 4.1", status: "Github Actions added" });
+  return res.json({ message: "Server is healthy this is v 4.5", status: "Github Actions added" });
 });
 
 app.listen(PORT, (req, res) => {
